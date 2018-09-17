ade42cdd4ad5c8c17eddba8adc34fc6840865c0e
mervyns <mervyns@gmail.com>
2018-09-17 16:34:36 +0800

    cleaned up some code for MVP

--- script.js
+++ script.js
@@ -85,30 +85,32 @@ let messageDisplay = document.getElementById("message-display");
 
 // Setting a function to call when Box is clicked.
 function clickBox() {
-  // Convert string to number
-  numberValue = parseInt(this.id);
-  // Add selected value to selected value array
-  selectedValues.push(numberValue);
-  // Add commas for thousands seperator
-  this.innerText = numberValue.toLocaleString();
-  this.classList.add("opened-box");
-  // Increment the Turn Number every time a box is clicked.
-  turnNumber++;
-  turnNumberDisplay.innerText = turnNumber;
-  // Cancelling out the opened box on the value table
-  for (i = 0; i < valueBox.length; i++) {
-    if (this.innerText === valueBox[i].innerText) {
-      valueBox[i].classList.add("strike-box");
+    // Check whether box is already opened. If opened, do not allow the player to click on it again.
+  if (this.classList.contains("opened-box")) {
+    messageDisplay.innerText =
+      "This box is already opened! Please choose another box!";
+  } else {
+    // Convert string to number
+    numberValue = parseInt(this.id);
+    // Add selected value to selected value array
+    selectedValues.push(numberValue);
+    // Add commas for thousands seperator
+    this.innerText = numberValue.toLocaleString();
+    this.classList.add("opened-box");
+    messageDisplay.innerText =
+      "The box you opened has got $" + numberValue.toLocaleString() + " in it.";
+    // Increment the Turn Number every time a box is clicked.
+    turnNumber++;
+    turnNumberDisplay.innerText = turnNumber;
+    // Cancelling out the opened box on the value table
+    for (i = 0; i < valueBox.length; i++) {
+      if (this.innerText === valueBox[i].innerText) {
+        valueBox[i].classList.add("strike-box");
+      }
     }
+    checkGame();
   }
-  checkGame();
 }
-
-const removeClicksOnGameBox = function() {
-  for (i = 0; i < gameBox.length; i++) {
-    gameBox[i].removeEventListener("click", clickBox);
-  }
-};
 
 // Adding Event Listener on each game box to listen for click event
 const allowClicksOnGameBox = function() {
@@ -116,40 +118,40 @@ const allowClicksOnGameBox = function() {
     gameBox[i].addEventListener("click", clickBox);
   }
 };
+// Add a function to remove the click listener to prevent players from clicking on additional boxes before making a decision
+const removeClicksOnGameBox = function() {
+  for (i = 0; i < gameBox.length; i++) {
+    gameBox[i].removeEventListener("click", clickBox);
+  }
+};
 
+// Initialize the playerDealDecision variable to log whether the player has made a decision or not
 let playerDealDecision;
 
 // Add Event Listener to Deal or No Deal Buttons to listen for click
+// Set/Toggle the playerDealDecision boolean based on player deal decision
 dealButton.onclick = function() {
   if (bankerHasAnOffer === true) {
     playerDealDecision = true;
-    messageDisplay.innerText = "Congratulations! You have chosen to sell your box to the banker for $" + parseInt(offerValue).toLocaleString()
+    messageDisplay.innerText =
+      "Congratulations! You have chosen to sell your box to the banker for $" +
+      parseInt(offerValue).toLocaleString();
     removeClicksOnGameBox();
-    dealCheck();
   }
 };
 
 noDealButton.onclick = function() {
   if (bankerHasAnOffer === true) {
     playerDealDecision = false;
-    messageDisplay.innerText = "OK. Please continue choosing."
+    messageDisplay.innerText = "You chose no deal! Please continue choosing!";
     allowClicksOnGameBox();
-    dealCheck();
   }
 };
 
 // Create Game Logic
-// Create Function to Log Deal or No Deal Value and proceeed or end the game depending on the choice.
-const dealCheck = function() {
-  if (playerDealDecision === true) {
-    console.log("true");
-  } else {
-    bankerHasAnOffer = false;
-    console.log("game continues");
-  }
-};
-
+// Call allowClicksOnGameBox function to allow clicks at the start of the game.
 allowClicksOnGameBox();
+// Check the turn number and give the values based on the turn number.
 const checkGame = function() {
   // Reduce the total amount in the array to a single value for easy calculation
   let totalPrizeMoney = prizeMoney.reduce(function(a, b) {
@@ -169,8 +171,7 @@ const checkGame = function() {
     turnNumber === 20
   ) {
     // Set the offerValue to be the average remaining value.
-    offerValue =
-      (totalPrizeMoney - totalSelectedValues) / (25 - turnNumber);
+    offerValue = (totalPrizeMoney - totalSelectedValues) / (25 - turnNumber);
     messageDisplay.innerText =
       "The banker called! He offered you $" +
       parseInt(offerValue).toLocaleString();
